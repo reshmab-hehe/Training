195cd6e69028e2df89ad9b46f590e84bc1f8450e
reshmab-hehe <153587415+reshmab-hehe@users.noreply.github.com>
2023-12-12 12:40:53 +0530

    Add files via upload

---
+++ x-mas/src/script.js
@@ -0,0 +1,23 @@
+var form = document.querySelector('#nameForm');
+var submitButton = document.querySelector('#submitButton');
+var wishPart = document.querySelector('#kirumasWish');
+var nameInput = document.querySelector('#name')
+var error = document.querySelector('#error');
+var wishMessage = document.querySelector('#wishes');
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    var nameValue = nameInput.value.trim();
+    if(nameValue === ''){
+      
+error.innerHTML= 'Please Enter Your Name';
+    }
+    else{
+    error.innerHTML= '';
+    wishPart.style.display = 'block';
+    wishMessage.append(nameValue);
+    nameInput.value ='';
+    }
+    
+});
