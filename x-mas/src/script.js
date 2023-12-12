a48a8c6c831200551d3650f858989cb37831cd4d
reshmab-hehe <153587415+reshmab-hehe@users.noreply.github.com>
2023-12-12 12:46:38 +0530

    Add files via upload

--- x-mas/src/script.js
+++ x-mas/src/script.js
@@ -7,7 +7,7 @@ var wishMessage = document.querySelector('#wishes');
 
 submitButton.addEventListener('click', (event) => {
     event.preventDefault();
-
+     wishMessage.innerHTML='';
     var nameValue = nameInput.value.trim();
     if(nameValue === ''){
       
